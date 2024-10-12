0ccd7dffe2af8cb477f292bb73e1eadb28b5ae02
sebteouii <sebteouiikhoubayb@gmail.com>
2024-10-12 05:13:40 +0200

    0. Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,20 +1,18 @@
-def canUnlockAll(boxes):
-    n = len(boxes)  
-    unlocked = [False] * n  
-    unlocked[0] = True  
-    keys = set(boxes[0])  
-    keys.add(0)  # Add the key for the first box
+#!/usr/bin/python3
 
-    
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = set(boxes[0])
+    keys.add(0)
+
     while keys:
-        new_key = keys.pop() 
-        if not unlocked[new_key]:
-            
+        new_key = keys.pop()
+        if new_key < n and not unlocked[new_key]:
             unlocked[new_key] = True
-           
             for key in boxes[new_key]:
-                if key < n:  
+                if key < n:
                     keys.add(key)
     
-   
     return all(unlocked)
